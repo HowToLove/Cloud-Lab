4091ae82e8aa58a105cb288b69574e6f1b0dd3f6
Rui Zhang <zhangruiangela@gmail.com>
2013-12-17 23:04:59 +0800

    首页0.9

--- cloud_lab/js/index.js
+++ cloud_lab/js/index.js
@@ -1,5 +1,8 @@
 $(document).ready(function () {
+	var height = $('.tab.show').height()+50;
+	$('#tab-container').height(height);
 
+	//主Tab切换
 	$('.tab-a').click(function(){
 		$('.tab-a').removeClass('active-a');
 		$(this).addClass('active-a');
@@ -17,15 +20,103 @@ $(document).ready(function () {
 		$(content).prevAll().addClass('hideleft');
 		$(content).nextAll().addClass('hideright');
 		$(content).addClass('show');
+		height = $(content).height()+50;
+		$('#tab-container').height(height);
+
 
 	});
-	$('.subtab section a').mouseover(function(){
+
+	//图片灰色和彩色切换
+	$('.subtab-a').mouseover(function(){
 		$(this).find('.cover').css("display","none");
 	});
-	$('.subtab section a').mouseout(function(){
+
+	$('.subtab-a').mouseout(function(){
 		$(this).find('.cover').css("display","inline-block");
 	});
 
+	//Subtab切换（课程列表与课程详情之间）
+	$('.subtab-a').click(function(){
+		var href = $(this).attr('href');
+		var parent = $(this).parents('.subtab');
+		var info = href + '-info';
+		parent.removeClass('show').addClass('hideright');
+		parent.prev().removeClass('hideleft').addClass('show');
+
+		$(info).addClass('show');
+		$(info).prevAll().addClass('hideleft');
+		$(info).nextAll().addClass('hideright');
+
+		if($(info).prevAll().length==0){
+			$('.btn-prev').css('opacity','.2');
+		}
+		if($(info).nextAll().length==0){
+			$('.btn-next').css('opacity','.2');
+		}
+
+		height=$(info).height()+50;
+		$('.subtab-container').height(height);
+	});
+
+	$('.btn-close').click(function(){
+		var parent = $(this).parents('.subtab');
+		parent.addClass('hideleft').removeClass('show');
+		parent.next().addClass('show').removeClass('hideright');
+		$('.btn-prev').css('opacity','1');
+		$('.btn-next').css('opacity','1');
+		$('.images').removeClass('hideright').removeClass('hideleft').removeClass('show');
+	});
+
+	//课程详情之间切换
+	$('.btn-next').click(function(){
+		var ithis=$('.images.show');
+		
+		if(ithis.nextAll().length==0){
+			$('.btn-next').css('opacity','.2');
+		}
+		else{
+			$('.btn-prev').css('opacity','1');
+			$('.btn-next').css('opacity','1');
+			$(ithis).addClass('hideleft').removeClass('show');
+			$(ithis).next().addClass('show').removeClass('hideright');
+			forbidbtn();
+		}
+	});
+	$('.btn-prev').click(function(){
+		var ithis=$('.images.show');
+		if(ithis.prevAll().length==0){
+		}
+		else{
+			$('.btn-prev').css('opacity','1');
+			$('.btn-next').css('opacity','1');
+			$(ithis).addClass('hideright').removeClass('show');
+			$(ithis).prev().addClass('show').removeClass('hideleft');
+			forbidbtn();
+		}
+	});
+
+	//登录注册切换
+	$('#btn-register').click(function(){
+		$('#form-login').removeClass('show').addClass('hideleft');
+		$('#form-register').removeClass('hideright').addClass('show');
+	})
+	$('#rbtn-back').click(function(){
+		$('#form-login').removeClass('hideleft').addClass('show');
+		$('#form-register').removeClass('show').addClass('hideright');
+	})
+
+	//判断左右箭头是否为灰
+	function forbidbtn(){
+		var ithis=$('.images.show');
+		if(ithis.prevAll().length==0){
+			$('.btn-prev').css('opacity','.2');
+		}
+		if(ithis.nextAll().length==0){
+			$('.btn-next').css('opacity','.2');
+		}
+	}
+
+	//AJAX请求
 	$.ajaxSetup({timeout : 6000});
 	// 登录ajax请求
 	$('#btn-login').click(function() {
