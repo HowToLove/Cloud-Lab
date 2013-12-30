82c89a06918fe8a13ee622f708832878492b4233
Rui Zhang <zhangruiangela@gmail.com>
2013-12-30 20:36:50 +0800

    布置作业完成

--- cloud_lab/js/PrepareClass.js
+++ cloud_lab/js/PrepareClass.js
@@ -3,4 +3,8 @@ $(document).ready(function(){
             $(this).next().toggle("normal");
             ($(this).html()=='查看答案') ? ($(this).html('隐藏答案')) : ($(this).html('查看答案'));
       });
+      $('.exer-list label').click(function(){
+            var checkmark = $(this).find('.checkmark');
+            checkmark.width()==0 ? checkmark.animate({width:"22px"}) : checkmark.animate({width:"0px"});
+      })
 });
